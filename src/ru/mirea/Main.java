d47e706ac672ba09fa1d9514967ac8ecba02ca24
1TSOP <nikita.postnov01@mail.ru>
2020-09-11 22:52:05 +0300

    Initial commit

---
+++ src/ru/mirea/Main.java
@@ -0,0 +1,36 @@
+package ru.mirea;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        //Shape
+         /*
+        Shape triangle = new Shape(3, "triangle", 5);
+        System.out.println(triangle.toString());
+    }
+
+         */
+        //Ball
+        /*
+        Ball ball = new Ball(4, "black");
+        System.out.println(ball.toString());
+         */
+        //Book
+        /*
+        Book book = new Book("Postnov", "Java", 750);
+        System.out.println(book.toString());
+    }
+         */
+        //Pitomnik +Dog
+        /*
+        Dog[] pitomnik = new Dog[3];
+        pitomnik[0] = new Dog("Red", 15);
+        pitomnik[1] = new Dog("Fufik", 3);
+        pitomnik[2] = new Dog("Shara");
+        pitomnik[2].setAge(20);
+        System.out.println(pitomnik[0]);
+        pitomnik[0].HumanAge();
+        pitomnik[1].HumanAge();
+        pitomnik[2].HumanAge();
+         */
+    }
+}
---
+++ src/ru/mirea/Shape.java
@@ -0,0 +1,28 @@
+package ru.mirea;
+public class Shape {
+    private int NumberOS;
+    private String NameOS;
+    private int lenght;
+    public Shape()
+    {
+    }
+    public Shape(int Num, String Name)
+    {
+        NumberOS = Num;
+        NameOS = Name;
+    }
+    public Shape(int numberOS, String nameOS, int lenght) {
+        NumberOS = numberOS;
+        NameOS = nameOS;
+        this.lenght = lenght;
+    }
+    @Override
+    public String toString() {
+        return "Shape{" +
+                "NumberOS=" + NumberOS +
+                ", NameOS='" + NameOS + '\'' +
+                ", lenght=" + lenght +
+                '}';
+    }
+}
+
